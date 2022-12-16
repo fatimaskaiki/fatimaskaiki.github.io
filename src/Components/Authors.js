41bae1b226680e7d6d8117416bf0131fb83a11bf
fatimaskaiki <skaikifatima96@gmail.com>
2022-12-16 12:50:12 +0200

    added search as you type

--- src/Components/Authors.js
+++ src/Components/Authors.js
@@ -1,50 +1,59 @@
 import React, { useState } from "react";
 import Card from "./BookCard";
 import { BiSearchAlt2 } from "react-icons/bi";
-import axios from 'axios'
+import axios from "axios";
+import ReactPaginate from 'react-paginate'
 
-function Author({user, setUser}) {
+function Author({ user, setUser }) {
   const [search, setSearch] = useState("");
-  const [bookData,setData]=useState([]);
-  const [allBooks, setAllBooks] = useState([]);
+  const [bookData, setData] = useState([]);
 
-  // const getAllBooks = (e) => {
-  //   axios.get('https://www.googleapis.com/books/v1/volumes?q' +
-  //   '&orderBy=newest' +
-  //   // '&filter=free-ebooks'+
-  //   '&key=AIzaSyDkfqaTnArpMe76VO0BWNIWdDbut1A4-XM')
-  //   .then(res=> setAllBooks(res.data.items))
-  //   console.log(allBooks)
-  // }
+  const [pageNumber, setPageNumber] = useState(0)
+  const booksPerPage = 30
+  const pagesVisited = pageNumber * booksPerPage 
+
+  const apiKey = "AIzaSyDkfqaTnArpMe76VO0BWNIWdDbut1A4-XM";
+
   const searchBook = (event) => {
-    if (event.key === "Enter") {
-      axios.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:'+search+
-      '&orderBy=newest' +
-      // '&filter=free-ebooks'+
-      '&key=AIzaSyDkfqaTnArpMe76VO0BWNIWdDbut1A4-XM'
-      // +'&maxResults=40'
-      )
-      .then(res=> setData(res.data.items))
-      .catch(err=>console.log(err))
+     if (search !== " ") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+            search +
+            "&orderBy=newest" +
+            // '&filter=free-ebooks'+
+            "&key=" +
+            apiKey
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err));
+    } else if (event.key === "Enter") {
+      axios.get(
+        "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+          search +
+          "&orderBy=newest" +
+          // '&filter=free-ebooks'+
+          "&key=" +
+          apiKey
+      ).then((res) => setData(res.data.items))
+      .catch((err) => console.log(err));
     }
   };
 
-//   const sortedBooks = 
-//   bookData.sort((a, b) => {
-//     return parseInt(b.volumeInfo.publishedDate) - parseInt(a.volumeInfo.publishedDate)
-// })
+  //   const sortedBooks =
+  //   bookData.sort((a, b) => {
+  //     return parseInt(b.volumeInfo.publishedDate) - parseInt(a.volumeInfo.publishedDate)
+  // })
 
-function handleSignOut(event) {
-  setUser({});
-  document.getElementById("signInDiv").hidden = false;
-}
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
 
   return (
     <>
       <div className="header">
-        <div className="row1">
-      
-        </div>
+        <div className="row1"></div>
         <div className="row2">
           <h2>Find Your Book </h2>
           <div id="search">
@@ -54,31 +63,32 @@ function handleSignOut(event) {
               value={search}
               onChange={(e) => setSearch(e.target.value)}
               onKeyPress={searchBook}
-              />
-              {console.log(search)}
+            />
             <button>
-              <BiSearchAlt2 style={{ fontSize: "30px", color: "rgb(245, 126, 57)", paddingTop: "6px" }} />
+              <BiSearchAlt2
+                style={{
+                  fontSize: "30px",
+                  color: "rgb(245, 126, 57)",
+                  paddingTop: "6px",
+                }}
+              />
             </button>
           </div>
           <div className="sign-out-button">
-      {Object.keys(user).length !== 0 && (
-        <p onClick={(e) => handleSignOut(e)}>Sign Out</p>
-        
-      )}
-    </div>
+            {Object.keys(user).length !== 0 && (
+              <p onClick={(e) => handleSignOut(e)}>Sign Out</p>
+            )}
+          </div>
         </div>
       </div>
       <div className="container">
-        {
-        //   search.length == 0 ?
-        // allBooks && 
-        // <Card book ={allBooks}/>
-        // : 
-        bookData &&
-        bookData.length !== 0 ?
-        <Card book={bookData} />
-        : <h3>Oops, this author doesn't exist!</h3>
-        }
+        {bookData && bookData.length !== 0 ? (
+          <Card book={bookData}
+          pagesVisited = {pagesVisited}
+          booksPerPage = {booksPerPage} />
+        ) : (
+          <h3>Oops, this author doesn't exist!</h3>
+        )}
       </div>
     </>
   );
